5a43495aab0da9ad424999259535f1a9a324e9e9
jstran9 <wptran58@gmail.com>
2019-10-04 18:49:49 -0700

    modifying configuration.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 app.use(express.static(__dirname + "/dist/"));
 
 app.get("*", function(req, res) {
-  res.sendFile(path.resolve(__dirname + "./src", "index.html"));
+  res.sendFile(path.resolve(__dirname + "/src", "index.html"));
 });
 
 app.listen(port);
