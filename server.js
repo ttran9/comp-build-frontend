272d5f50f19633d3122d2960122c260bb0077533
jstran9 <wptran58@gmail.com>
2019-10-04 18:43:27 -0700

    modifying configuration.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/dist/"));
 
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/src/index.html"));
+  res.sendFile(__dirname + "/src/index.html");
 });
 
 app.listen(port);
