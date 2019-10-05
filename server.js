388b4b8e2e4a23defff44a389842d9b8e225b95c
jstran9 <wptran58@gmail.com>
2019-10-05 10:39:31 -0700

    modifying configuration.

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
 
-app.use(express.static(__dirname + "/dist/"));
+const frontend = path.join(__dirname, "dist/");
+
+// map the requests to the static react build directory
+app.use("/", express.static(frontend));
+
+// all the unknown requests are redirected to the react SPA
+app.use(function(req, res, next) {
+  res.sendFile(path.join(frontend, "index.html"));
+});
 
 app.listen(port);
