f318322c4813b154a8beaa3bf93b23d60aff5e27
jstran9 <wptran58@gmail.com>
2019-10-04 18:47:16 -0700

    modifying configuration.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require("path");
 
 app.use(express.static(__dirname + "/dist/"));
 
 app.get("*", function(req, res) {
-  res.sendFile(__dirname + "/src/index.html");
+  res.sendFile(path.resolve(__dirname + "./src", "index.html"));
 });
 
 app.listen(port);
