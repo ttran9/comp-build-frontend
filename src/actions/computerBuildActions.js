2883f259d7952068af1b7a51a36ed84e1b16c743
jstran9 <wptran58@gmail.com>
2019-10-09 19:43:46 -0700

    testing process env

--- src/actions/computerBuildActions.js
+++ src/actions/computerBuildActions.js
@@ -48,6 +48,7 @@ export const getComputerBuildByBuildIdentifier = buildIdentifier => async dispat
 };
 
 export const getComputerBuilds = () => async dispatch => {
+  console.log(process.env);
   try {
     const response = await axios.get(Constants.COMPUTER_BUILD_API);
     dispatch({
