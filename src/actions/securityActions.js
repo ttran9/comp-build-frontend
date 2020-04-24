62baba85cb37e74614b63b675c744865c116141c
jstran9 <wptran58@gmail.com>
2020-04-24 01:08:31 -0700

    modifying formating (no functional change).

--- src/actions/securityActions.js
+++ src/actions/securityActions.js
@@ -1,15 +1,21 @@
 import axios from "axios";
 import * as Constants from "../Constants";
-import { GET_ERRORS, SET_CURRENT_USER, REQUEST_SUCCESS } from "./types";
+import {
+  GET_ERRORS,
+  SET_CURRENT_USER,
+  REQUEST_SUCCESS
+} from "./types";
 import setJWTToken from "../securityUtils/setJWTToken";
 import jwt_decode from "jwt-decode";
-import { clearErrors } from "../errorUtils/clearErrors";
+import {
+  clearErrors
+} from "../errorUtils/clearErrors";
 
-export const createNewUser = (newUser, history) => async dispatch => {
+export const createNewUser = (newUser, history) => async (dispatch) => {
   try {
     await axios
       .post(`${Constants.REGISTER_ENDPOINT}`, newUser)
-      .then(response => {
+      .then((response) => {
         /*
          * dispatch an action to update the state with the success message.
          * note there are other objects inside of the response.data but we only need to view the message key/* value pair.
@@ -37,7 +43,7 @@ export const createNewUser = (newUser, history) => async dispatch => {
   }
 };
 
-export const login = LoginRequest => async dispatch => {
+export const login = (LoginRequest) => async (dispatch) => {
   try {
     // post to login endpoint with LoginRequest payload object
     const response = await axios.post(
@@ -48,7 +54,9 @@ export const login = LoginRequest => async dispatch => {
      * extract token from the response.data
      * note: there is also a success object only the token object is required.
      */
-    const { token } = response.data;
+    const {
+      token
+    } = response.data;
     // store the token in the localStorage
     localStorage.setItem(`${Constants.JWT_TOKEN}`, token);
 
@@ -72,7 +80,7 @@ export const login = LoginRequest => async dispatch => {
   }
 };
 
-export const logout = () => dispatch => {
+export const logout = () => (dispatch) => {
   localStorage.removeItem(`${Constants.JWT_TOKEN}`);
   setJWTToken(false); // delete the Authorization header ("Bearer [token here]").
   dispatch({
@@ -83,12 +91,12 @@ export const logout = () => dispatch => {
   });
 };
 
-export const confirmRegistration = (token, history) => async dispatch => {
+export const confirmRegistration = (token, history) => async (dispatch) => {
   try {
     // make a post request with the token in order to activate the account.
     await axios
       .post(`${Constants.CONFIRM_REGISTRATION_ENDPOINT}/${token}`)
-      .then(response => {
+      .then((response) => {
         dispatch({
           /*
            * dispatch an action to update the state with the success message.
@@ -108,14 +116,16 @@ export const confirmRegistration = (token, history) => async dispatch => {
   }
 };
 
-export const requestPasswordChange = (history, username) => async dispatch => {
+export const requestPasswordChange = (history, username) => async (
+  dispatch
+) => {
   try {
     /*
      * the idea here is to redirect the user to a page that informs them the password request was successful * and to check his/her email with instructions.
      */
     await axios
       .post(`${Constants.CHANGE_PASSWORD_ENDPOINT}/`, username)
-      .then(response => {
+      .then((response) => {
         dispatch({
           type: REQUEST_SUCCESS,
           payload: response.data.message
@@ -132,21 +142,20 @@ export const requestPasswordChange = (history, username) => async dispatch => {
   }
 };
 
-export const changePassword = (
-  token,
-  history,
-  passwordContents
-) => async dispatch => {
+export const changePassword = (token, history, passwordContents) => async (
+  dispatch
+) => {
   try {
     // passwordContents has a password and a confirmPassword key/value pair.
     await axios
       .post(`${Constants.CHANGE_PASSWORD_ENDPOINT}/${token}`, passwordContents)
-      .then(response => {
+      .then((response) => {
         // dispatch an action to update the state with the success message.
         dispatch({
           type: REQUEST_SUCCESS,
           payload: response.data.message
         });
+        // TODO: look for an efficient way to redirect and keep the state in tact so user is shown the message that the password has changed.
         // redirect to a page that will display that the password has been changed successfully.
         history.push(`${Constants.REQUEST_SUCCESS_URL}`);
       });
